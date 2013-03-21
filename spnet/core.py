c5cfc7cd4d58eaeeb8b8c61b6cc848ebb2b8d48e
Christopher Lee <leec@chem.ucla.edu>
2013-03-20 18:05:12 -0700

    fixed typo

--- spnet/core.py
+++ spnet/core.py
@@ -105,7 +105,7 @@ class Post(UniqueArrayDocument):
     parent = LinkDescriptor('parent', fetch_parent_paper, noData=True)
     author = LinkDescriptor('author', fetch_person)
     def get_replies(self):
-        for r in getattr(self.parent, 'replies' ()):
+        for r in getattr(self.parent, 'replies', ()):
             if r.replyTo == self:
                 yield r
 
