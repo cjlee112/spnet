fa59c15ad8c5ea2ae65fc82b4d16e44f19238c48
Christopher Lee <leec@chem.ucla.edu>
2013-03-21 13:01:53 -0700

    improved arxiv ID handling and added simple block-result search function

--- spnet/arxiv.py
+++ spnet/arxiv.py
@@ -4,8 +4,26 @@ import twitter
 from time import mktime
 from datetime import datetime
 import urllib
+import re
 
 arxivApiUrl = 'http://export.arxiv.org/api/query?'
+
+def get_arxiv_id(arxivURL):
+    'extract arxivID from arxiv URL, replacing / --> _'
+    l = arxivURL.split('/')
+    s = '_'.join(l[4:])
+    try:
+        return s[:s.rindex('v')] # remove version info
+    except ValueError:
+        return s
+
+def is_id_string(s,
+                 dottedNumber=re.compile(
+                     r'[0-9][0-9][0-9]+\.[0-9]+[0-9v][0-9]+$'),
+                 fieldSlashNumber=re.compile(
+                     '[a-z][a-z]+[a-z-][a-z]+/[0-9]+[0-9v][0-9]+$')):
+    'True if s looks like an arXiv paper string'
+    return dottedNumber.match(s) or fieldSlashNumber.match(s)
 
 def lookup_papers(id_list, **kwargs):
     'retrieve a list of arxiv IDs, as a generator function'
@@ -15,11 +33,23 @@ def lookup_papers(id_list, **kwargs):
         url = arxivApiUrl + '&'.join(['%s=%s' % t for t in d.items()])
         f = feedparser.parse(url)
         for e in f.entries:
-            l = e['id'].split('/')
-            e['id'] = l[4].split('v')[0] # replace URL by arxivID
+            e['id'] = get_arxiv_id(e['id']) # replace URL by arxivID
             yield e
 
-def search_arxiv(search_query, block_size=25):
+def search_arxiv(searchString, start=0, block_size=25):
+    'retrieve list of block_size results for specified search'
+    q = dict(search_query=searchString, max_results=str(block_size),
+             start=str(start))
+    url = arxivApiUrl + urllib.urlencode(q)
+    f = feedparser.parse(url)
+    l = []
+    for e in f.entries:
+        e['id'] = get_arxiv_id(e['id'])
+        l.append(e)
+    return l
+
+
+def search_arxiv_iter(search_query, block_size=25):
     'iterate over arxiv papers matching search_query'
     start = 0
     q = dict(search_query=search_query, max_results=str(block_size))
@@ -28,6 +58,7 @@ def search_arxiv(search_query, block_size=25):
         url = arxivApiUrl + urllib.urlencode(q)
         f = feedparser.parse(url)
         for e in f.entries:
+            e['id'] = get_arxiv_id(e['id']) # replace URL by arxivID
             yield e
         start += block_size
         if len(f.entries) < block_size:
