2c9e064b65c9dfb48ce182ad1714ef4522b4c0d3
Christopher Lee <leec@chem.ucla.edu>
2013-03-31 00:26:00 -0700

    added support for multipage pubmed searching and xmltodict content extraction

--- spnet/pubmed.py
+++ spnet/pubmed.py
@@ -2,6 +2,72 @@ from lxml import etree
 import xmltodict
 import requests
 
+def bfs_search_results(d, searches, results):
+    '''find all keys in searches, and save values to results.
+    WARNING: deletes keys from searches dict!'''
+    try:
+        l = d.items()
+    except AttributeError:
+        return
+    for k, v in l:
+        try:
+            f = searches[k] # found a match
+        except KeyError:
+            continue
+        if callable(f): # let f generate whatever results it wants
+            try:
+                r = f(v, k, results)
+            except KeyError: # didn't find what it wanted, so ignore
+                pass
+            else:
+                results.update(r)
+        elif f: # save with alias key
+            results[f] = v
+        else: # save with original key
+            results[k] = v
+        del searches[k]
+        if not searches: # nothing more to do
+            return
+    for k, v in l:
+        bfs_search_results(v, searches, results)
+        if not searches: # nothing more to do
+            return
+
+def bfs_search(d, searches):
+    'find all keys in searches, and save values to results.'
+    results = {}
+    bfs_search_results(d, searches.copy(), results)
+    return results
+
+def get_val(v, k, r):
+    return {k:v['#text']}
+
+def get_author_names(v, k, r):
+    return {'authorNames':[d['ForeName'] + ' ' + d['LastName'] for d in v]}
+
+def list_wrap(v):
+    if isinstance(v, list):
+        return v
+    else:
+        return [v]
+
+def get_doi(v, k, r):
+    for d in list_wrap(v):
+        if d.get('@IdType', '') == 'doi':
+            return {'doi':d['#text']}
+    raise KeyError('no doi found')
+
+pubmedExtracts = dict(ArticleTitle='title', AbstractText='summary',
+                      PMID=lambda v,k,r:{'id':v['#text']},
+                      ArticleDate=lambda v,k,r:{'year':v['Year']},
+                      ISOAbbreviation='journal', ISSN=None,
+                      Affiliation='affiliation', Author=get_author_names,
+                      ArticleId=get_doi)
+
+def normalize_pubmed_dict(d, extracts=pubmedExtracts):
+    return bfs_search(d, extracts)
+
+
 def extract_subtrees(xml, extractDicts):
     'extract subtrees as OrderedDict objects'
     d = {}
@@ -104,4 +170,22 @@ def get_training_abstracts(terms=('cancer', 'transcription', 'evolution',
         root = etree.XML(xml)
         for o in root.findall('.//AbstractText'):
             yield o.text
+
+class PubmedSearch(object):
+    def __init__(self, searchString, block_size=20, **kwargs):
+        self.block_size =  block_size
+        xml = search_pubmed(searchString, usehistory='y', tool=None,
+                            email=None, retmax=str(block_size), **kwargs)
+        d, root = dict_from_xml(xml, WebEnv='!WebEnv', query_key='!QueryKey')
+        self.queryArgs = d
+    def __call__(self, searchString, start=0, block_size=20):
+        'get list of PubmedArticle dicts'
+        xml = query_pubmed(retstart=str(start), retmax=str(block_size),
+                           tool=None, email=None, **self.queryArgs)
+        d = extract_subtrees(xml, ('PubmedArticleSet.PubmedArticle'.split('.'),))
+        l = []
+        for dd in d['PubmedArticle']:
+            dd.update(normalize_pubmed_dict(dd))
+            l.append(dd)
+        return l
 
