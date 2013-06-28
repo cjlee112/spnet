5e1bdab9555323329672b608bcee883a551c7b1a
Christopher Lee <leec@chem.ucla.edu>
2013-06-27 23:12:34 -0700

    added bulk delivery of Recs db calls

--- spnet/bulk.py
+++ spnet/bulk.py
@@ -1,6 +1,6 @@
 import core
 
-def get_people_topics():
+def find_people_topics():
     'construct dict of person:topics from Recs, Posts, PaperInterests'
     people = {}
     for r in core.Recommendation.find_obj():
@@ -31,3 +31,46 @@ def insert_people_topics(peopleTopics):
     for personID,topics in peopleTopics.items():
         core.Person.coll.update({'_id':personID},
                                 {'$addToSet': {'topics': {'$each':list(topics)}}})
+
+def get_people_subs():
+    topics = {}
+    subs = {}
+    for d in core.Person.find({}, {'topics':1, 'topicOptions':1, 'subscriptions':1}):
+        personID = d['_id']
+        hideSet = set([dd['topic'] for dd in d.get('topicOptions', ())
+                       if dd.get('fromOthers', 0) == 'hide'])
+        for topic in d.get('topics', ()): # index topics this person wants to get
+            if topic in hideSet: # user doesn't want all recs on this topic
+                continue
+            try:
+                topics[topic].append(personID)
+            except KeyError:
+                topics[topic] = [personID]
+        for sub in d.get('subscriptions', ()): # index people this person wants to get
+            author = sub['author']
+            try:
+                subs[author].append(personID)
+            except KeyError:
+                subs[author] = [personID]
+    return topics, subs
+
+
+
+def deliver_recs(topics, subs):
+    for d in core.Paper.find({'recommendations': {'$exists':True}}, 
+                             {'recommendations':1}):
+        paperID = d['_id']
+        for r in d['recommendations']:
+            author = r['author']
+            sigs = r.get('sigs', ())
+            for personID in subs.get(author, ()): # deliver to subscribers
+                core.Person.coll.update({'_id':personID},
+                    {'$addToSet': {'received': 
+                                   {'paper':paperID, 'from':author, 'topics':sigs}}})
+            for topic in sigs:
+                for personID in topics.get(topic, ()): # deliver to subscribers
+                    if personID == author: # don't deliver back to author!
+                        continue
+                    core.Person.coll.update({'_id':personID},
+                        {'$addToSet': {'received': 
+                                  {'paper':paperID, 'from':author, 'topics':sigs}}})
