9d116e9abebb227124198a5515c25c9d89e83ff9
Christopher Lee <leec@chem.ucla.edu>
2013-10-10 16:45:39 -0700

    added tests for URL based arxiv ID recognition and old arxiv ID dedupe

--- spnet/test_incoming.py
+++ spnet/test_incoming.py
@@ -5,7 +5,8 @@ from datetime import datetime
 post1 = dict(
     id='temporaryPost',
     content='''this is my post.
-It talks about two papers including  arXiv:0910.4103
+It talks about three papers including  arXiv:0910.4103
+and http://arxiv.org/abs/1310.2239
 #spnetwork arXiv:0804.2682 #gt  arXiv:0910.4103 arXiv:0804.2682 #gt''',
     user='114744049040264263224',
     title='Such an interesting Post!',
@@ -24,12 +25,15 @@ def test1():
     assert len(l) == 1
     post = l[0]
     assert post.parent.arxiv.id == '0804.2682'
-    assert len(post.citations) == 1
-    assert post.citations[0].parent.arxiv.id == '0910.4103'
+    assert len(post.citations) == 2
+    ids = [c.parent.arxiv.id for c in post.citations]
+    assert '0910.4103' in ids and '1310.2239' in ids
     assert len(post.citations[0].parent.citations) == 1
     assert post.citations[0].title == 'Such an interesting Post!'
+    assert len(post.citations[1].parent.citations) == 1
     # finally clean up by deleting our test post
     post.citations[0].delete()
+    post.citations[1].delete()
     post.delete()
     print 'tests passed'
 
@@ -41,3 +45,5 @@ def test_arxiv_versions(arxivIDs=('1108.1172', '1108.1172v3')):
             raise AssertionError('arxiv versions map to different Paper records!')
     print 'tests passed.'
 
+def test_arxiv_versions2(arxivIDs=('math.HO_9404236', 'math.HO_9404236')):
+    test_arxiv_versions(arxivIDs)
