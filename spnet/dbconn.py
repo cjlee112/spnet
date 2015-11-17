645daa5d7d00f366490e4d0fc61257c45a5abf93
Christopher Lee <leec@chem.ucla.edu>
2015-11-17 10:30:17 -0800

    must authenticate non-admin user

--- spnet/dbconn.py
+++ spnet/dbconn.py
@@ -3,14 +3,15 @@ import pymongo
 
 class DBConnection(object):
     'store different collection objects on specified classes'
-    def __init__(self, classDict, user=None, password=None, **kwargs):
+    def __init__(self, classDict, user=None, password=None, dbname='spnet',
+                 **kwargs):
         '''Each keyword argument specifies an attr:collection pair.
         If collection is a string, it must be of the form dbname.collname.
         Otherwise it must be a collection object to be saved as-is.'''
         self._conn = pymongo.connection.Connection(**kwargs)
-        if user == 'admin': # authenticating to admin DB gives access to all DB
-            adminDB = self._conn['admin']
-            adminDB.authenticate(user, password)
+        if dbname: # authenticating to access DB
+            accessDB = self._conn[dbname]
+            accessDB.authenticate(user, password)
         for klass, v in classDict.items():
             if isinstance(v, str):
                 db, coll = v.split('.')
